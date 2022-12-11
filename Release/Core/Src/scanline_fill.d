5c363b7d7a6081d7273202aab509f589a97a5157
Bojan Sofronievski <bojan.drago@gmail.com>
2022-12-11 20:47:17 +0100

    Added Release target

---
+++ Release/Core/Src/scanline_fill.d
@@ -0,0 +1,8 @@
+Core/Src/scanline_fill.o: ../Core/Src/scanline_fill.c \
+ ../Core/Inc/scanline_fill.h ../Core/Inc/color.h ../Core/Inc/point.h
+
+../Core/Inc/scanline_fill.h:
+
+../Core/Inc/color.h:
+
+../Core/Inc/point.h:
---
+++ Release/Core/Src/subdir.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/artist.c \
+../Core/Src/color.c \
+../Core/Src/cycle_counter.c \
+../Core/Src/font.c \
+../Core/Src/gpio.c \
+../Core/Src/lcd_ili9488.c \
+../Core/Src/main.c \
+../Core/Src/point.c \
+../Core/Src/scanline_fill.c \
+../Core/Src/spi.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f4xx.c 
+
+OBJS += \
+./Core/Src/artist.o \
+./Core/Src/color.o \
+./Core/Src/cycle_counter.o \
+./Core/Src/font.o \
+./Core/Src/gpio.o \
+./Core/Src/lcd_ili9488.o \
+./Core/Src/main.o \
+./Core/Src/point.o \
+./Core/Src/scanline_fill.o \
+./Core/Src/spi.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Core/Src/artist.d \
+./Core/Src/color.d \
+./Core/Src/cycle_counter.d \
+./Core/Src/font.d \
+./Core/Src/gpio.d \
+./Core/Src/lcd_ili9488.d \
+./Core/Src/main.d \
+./Core/Src/point.d \
+./Core/Src/scanline_fill.d \
+./Core/Src/spi.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
